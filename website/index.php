d1f33b6514810acac601c35da1374515f2761916
Agam Aulakh <aulakhagam@gmail.com>
2021-12-18 23:06:20 -0700

    moved index

---
+++ website/index.php
@@ -0,0 +1,71 @@
+
+<?php
+
+?>
+
+<head>
+  <meta charset="utf-8">
+  <title>Hospital Volunteer Database Management System</title>
+  <link rel="icon" href="images/webIcon.ico">
+  <link rel="stylesheet" href="css/styles.css">
+  <!-- font -->
+  <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Notable&display=swap" rel="stylesheet">
+  <!-- Bootstrap-->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+
+
+<section id="title">
+
+
+    <div class="container-fluid">
+        <h1>Hospital Volunteer Database Management System</h1>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="row">
+            <div class="col-lg-6">
+                <br><br>
+                <a href="signin.php" class="btn btn-Danger btn-lg btn-block" role="button"></i> Please Sign In</a>
+                <br><br>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <h1>Or Sign Up:</h1>
+            </div>
+            <div class="col-lg-8">
+                <a href="i-volunt/signup.php" class="btn btn-warning btn-sm" role="button"> </i> Internal Volunteer </a>
+                <a href="e-volunt/signup.php" class="btn btn-warning btn-sm" role="button"> </i>  External Volunteer </a>
+                <a href="p-volunt/signup.php" class="btn btn-warning btn-sm" role="button"> </i>  Prospective Volunteer </a>
+                <a href="coordinator/signup.php" class="btn btn-warning btn-sm" role="button"> </i> Coordinator </a>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+  <!-- Footer -->
+    <footer class="white-section" id="footer">
+        <br><br>
+        <div class="container-fluid">
+        <p>© Copyright 2021 Group 39 CPSC 471 </p>
+        </div>
+    </footer>
+
+</body>
